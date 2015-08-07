1717bc5d82ae1078712cc1f906091ba6255597b5
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-06 20:38:31 -0500

    Added urlroot to model

--- assets/js/models/directory.js
+++ assets/js/models/directory.js
@@ -1,11 +1,12 @@
-// Defined a model class and attached it to collection
+// Defined a model class
 var Person = Backbone.Model.extend({
+  urlRoot: 'http://tiny-lr.herokuapp.com/collections/ms-directory',
   idAttribute: '_id',
   defaults: {
-    firstName: '',
-    lastName: '',
+    firstname: '',
+    lastname: '',
     address: '',
-    phoneNum: '',
+    phonenum: '',
   },
 });
 
