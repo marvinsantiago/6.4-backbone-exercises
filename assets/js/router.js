f22280f3d8041a8941259ecf675c815cd936a7db
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-10 20:23:22 -0500

    Data still not hitting the page

--- assets/js/router.js
+++ assets/js/router.js
@@ -8,5 +8,12 @@ var AppRouter = Backbone.Router.extend({
     ':id/view': 'view',
   },
 
+  view: function(id) {
+    var _this = this;
+    this.collection.fetch().then(function() {
+      var html = _this.template(_this.collection.get(id).toJSON());
+      $('#target').html(html);
+    });
+  },
 });
 
