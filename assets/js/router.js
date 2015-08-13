dae095cdf50e9c9a2cb15221be655bd2d9ca7d03
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-13 16:56:15 -0500

    Add initialize function to router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,3 +1,7 @@
 var AppRouter = Backbone.Router.extend({
+  initialize: function() {
+    this.collection = new AllBookmarks();
+    this.itemview = new ItemView({collection: this.collection});
+  },
 
 });
